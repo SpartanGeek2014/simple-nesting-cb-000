bc87fc85dbb8a9c25ae548133bbc58af133fefe7
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-20 20:25:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -59,10 +60,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+ #:yukihiro_matsumoto => {
+  # :known_for => "Ruby",
+  # :languages => ["LISP", "C"]
+ #}
 
 	programmer_hash = 
  		{
@@ -79,8 +80,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+		 programmer_hash:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
 end
 
 def changing_alan
@@ -102,7 +103,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
